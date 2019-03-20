c6a3dc2100f0317016fd567a4993fb8e5e666832
Nonso Mgbechi <mgbechinonso@gmail.com>
2019-03-21 00:21:36 +0100

    Added the resource PostsController routes and the home to a route group

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
+    Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('/posts', 'PostsController');
-
+    Route::resource('/posts', 'PostsController');
+});
