4733d635215caed2969e0c2cbcfc3ead4c28a3cd
Nonso Mgbechi <mgbechinonso@gmail.com>
2019-03-22 23:58:44 +0100

    Added fillable fields to both models and accessor for 'featured' field to Post

--- app/Post.php
+++ app/Post.php
@@ -3,9 +3,24 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+    use SoftDeletes;
+
+    protected $fillable = [
+        'title', 'content', 'featured', 'category_id', 'slug'
+    ];
+
+    protected $dates = ['deleted_at'];
+
+    // Accessor to mutate featured field in views
+    public function getFeaturedAttribute ($featured)
+    {
+        return asset($featured);
+    }
+
     public function category ()
     {
         return $this->belongsTo('App\Category');
