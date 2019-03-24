a5e73584d4bbeca19aea562add0898e592f0e2d9
Nonso Mgbechi <mgbechinonso@gmail.com>
2019-03-24 20:43:00 +0100

    Added fillable attribute

--- app/Profile.php
+++ app/Profile.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+
+    protected $fillable = [
+        'user_id', 'avatar', 'facebook', 'about', 'youtube'
+    ];
+
     public function user ()
     {
         return $this->belongsTo('App\User');
