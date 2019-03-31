163d8b08064314e9db4bc49c2d11b80eedb827dc
Nonso Mgbechi <mgbechinonso@gmail.com>
2019-03-31 16:53:56 +0100

    Sorted categories according to their number of posts and passed the top four to the view in the index method

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -11,8 +11,28 @@ class FrontendController extends Controller
 {
     public function index()
     {
+        $categories = Category::all();
+
+        // Use Collection sort method to sort Categories according to the
+        // number of posts they have, if we have some Categories
+
+        if ( ! $categories->isEmpty() ) {
+            $categories->sort(function ($a, $b) {
+                $aCount = $a->posts()->count();
+                $bCount = $b->posts()->count();
+                if ($aCount === $bCount) {
+                    return 0;
+                }
+                return ($aCount > $bCount) ? 1 : -1;
+            });
+
+            // Take the top four categories
+
+            $categories = $categories->take(4);
+        }
+
         return view('welcome')
-                    ->with('categories', Category::take(4)->get())
+                    ->with('categories', $categories)
                     ->with('first_post', Post::orderBy('created_at', 'DESC')->first())
                     ->with('second_post', Post::orderBy('created_at', 'DESC')->skip(1)->take(1)->get()->first())
                     ->with('third_post', Post::orderBy('created_at', 'DESC')->skip(2)->take(1)->get()->first())
