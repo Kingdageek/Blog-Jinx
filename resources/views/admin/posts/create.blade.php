1664dfbe3cea114769ce8cae21c06172e6c85d9c
Nonso Mgbechi <mgbechinonso@gmail.com>
2019-04-01 00:54:35 +0100

    Made sure category user previously selected remains selected if an error causes a redirect back

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -26,7 +26,9 @@
                     <label for="category_id">Select a category</label>
                     <select name="category_id" id="category_id" class="form-control">
                         @foreach ($categories as $category)
-                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                        <option value="{{ $category->id }}"
+                        {{ $category->id == old('category_id') ? 'selected' : ''}}
+                        >{{ $category->name }}</option>
                         @endforeach
                     </select>
                 </div>
