2776b3149076629fedb10857505447574ddd26e7
Nonso Mgbechi <mgbechinonso@gmail.com>
2019-04-09 00:54:23 +0100

    Renamed home to dashboard and created basic dashboard CMS summary info

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-3">
+            <div class="panel panel-info">
+                <div class="panel-heading text-center">
+                    PUBLISHED POSTS
+                </div>
+                <div class="panel-body">
+                    <h1 class="text-center">{{ $posts_count }}</h1>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3">
+            <div class="panel panel-danger">
+                <div class="panel-heading text-center">
+                    THRASHED POSTS
+                </div>
+                <div class="panel-body">
+                    <h1 class="text-center">{{ $trashed_count }}</h1>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3">
+            <div class="panel panel-success">
+                <div class="panel-heading text-center">
+                    USERS
+                </div>
+                <div class="panel-body">
+                    <h1 class="text-center">{{ $users_count }}</h1>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3">
+            <div class="panel panel-primary">
+                <div class="panel-heading text-center">
+                    CATEGORIES
+                </div>
+                <div class="panel-body">
+                    <h1 class="text-center">{{ $categories_count }}</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+@endsection
